5b6c64f5d4bd8e5a226b7465c6848d0b5ebba277
strong-ge <simba_cc@163.com>
2016-03-27 15:20:07 +0800

    collect static

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -102,6 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '../static/'
 MEDIA_ROOT='/home/zjq/upload/'
 MEDIA_URL='/upload/'
 
